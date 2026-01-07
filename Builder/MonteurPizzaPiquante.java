0023f9a1dfbf70d7d2102759e17f2e449b52caca
dtsabeng <delphantsabeng@yahoo.com>
2026-01-07 08:49:23 +0100

    Suppression d'un dossier complementaire

--- Builder/MonteurPizzaPiquante.java
+++
@@ -1,5 +0,0 @@
-class MonteurPizzaPiquante extends MonteurPizza {
-    public void monterPate() { pizza.setPate("feuilletée"); }
-    public void monterSauce() { pizza.setSauce("piquante"); }
-    public void monterGarniture() { pizza.setGarniture("pepperoni+salami"); }
-}
